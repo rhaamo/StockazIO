6228f87c370626de7eb4bb8fb7eba9879a58ef63
Dashie <dashie@sigpipe.me>
2019-12-30 14:58:16 +0100

    Switch to select2 instead of chosen

--- api/controllers/part/static/js/global.js
+++ api/controllers/part/static/js/global.js
@@ -1,17 +1,9 @@
 $(document).ready(function () {
     $(function () {
-        // https://harvesthq.github.io/chosen/options.html
-        // Forcing some defaults
-        let chosenOpts = {
-            disable_search: false,
-            enable_split_word_search: true,
-            search_contains: true,
-            case_sensitive_search: false
-        };
-        $('#id_part_unit.select').chosen(chosenOpts);
-        $('#id_footprint.select').chosen(chosenOpts);
-        $('#id_storage.select').chosen(chosenOpts);
-        $('#id_category.select').chosen(chosenOpts);
+        $('#id_part_unit.select').select2();
+        $('#id_footprint.select').select2();
+        $('#id_storage.select').select2();
+        $('#id_category.select').select2();
 
 
         $('.formset_row-distributors_sku').formset({
@@ -24,10 +16,10 @@ $(document).ready(function () {
             deleteText: 'remove',
             prefix: 'manufacturers_sku',
         });
-        $('.formset_row-part_parameters').formset({
+        $('.formset_row-part_parameters_value').formset({
             addText: 'add one',
             deleteText: 'remove',
-            prefix: 'part_parameters',
+            prefix: 'part_parameters_value',
         });
     });
 });
