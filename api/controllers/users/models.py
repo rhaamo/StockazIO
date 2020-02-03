53529c39b3dcdb7111a7078196df4dde3fa046b4
Dashie <dashie@sigpipe.me>
2020-02-03 10:26:37 +0100

    unused

--- api/controllers/users/models.py
+++ api/controllers/users/models.py
@@ -13,15 +13,3 @@ default_permissions = ["categories", "parts"]
 class User(AbstractUser):
     class Meta:
         db_table = "auth_user"
-
-    def get_permissions(self, defaults=None):
-        defaults = defaults or default_permissions
-        perms = {}
-        for p in PERMISSIONS:
-            v = self.is_superuser or p in defaults
-            perms[p] = v
-        return perms
-
-    @property
-    def all_permissions(self):
-        return self.get_permissions()
