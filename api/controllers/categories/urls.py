29aa1857c6392359dc4d98ba09072ef5f88d73ee
Dashie <dashie@sigpipe.me>
2020-10-26 15:30:31 +0100

    coin

--- api/controllers/categories/urls.py
+++ api/controllers/categories/urls.py
@@ -2,6 +2,6 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r"categories", views.CategoryViewSet, basename="Category")
+router.register(r"", views.CategoryViewSet, basename="Category")
 
 urlpatterns = router.urls + []
