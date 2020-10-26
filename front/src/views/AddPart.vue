9806993f3e805abc867b6e2fec9432aa8fd25166
Dashie <dashie@sigpipe.me>
2020-10-26 22:00:08 +0100

    coin

--- front/src/views/AddPart.vue
+++ front/src/views/AddPart.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="add_part">
-    <h1>Add a part</h1>
+    <div class="row">
+      <div class="col-lg-9">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><router-link :to="{name: 'home'}">StockazIO</router-link></li>
+          <li class="breadcrumb-item active">Add new</li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-11 mx-auto">
+        form here
+      </div>
+    </div>
+
   </div>
 </template>
