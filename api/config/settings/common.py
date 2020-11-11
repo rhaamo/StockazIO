88c596253e5da299e3ae716eaedb28e92415838d
Dashie <dashie@sigpipe.me>
2020-11-11 13:27:19 +0100

    Disable unused auth

--- api/config/settings/common.py
+++ api/config/settings/common.py
@@ -331,8 +331,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",  # disable thoses or we will have
+        # "rest_framework.authentication.SessionAuthentication",  # some annoying CSRF issues
     ),
     "DEFAULT_PERMISSION_CLASSES": ("controllers.oauth.permissions.ScopePermission",),
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
