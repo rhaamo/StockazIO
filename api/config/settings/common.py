cfed82e65f82e05c83427b4b2a904520ca5de962
Dashie <dashie@sigpipe.me>
2020-11-17 09:49:34 +0100

    unused

--- api/config/settings/common.py
+++ api/config/settings/common.py
@@ -231,7 +231,6 @@ TEMPLATES = [
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
-                "dealer.contrib.django.context_processor"
                 # Your stuff: custom template context processors go here
             ],
         },
