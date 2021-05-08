2ef5751fd90f320f24fd60c1a4e0a4e6ba6d436e
Dashie <dashie@sigpipe.me>
2021-05-08 09:03:44 +0200

    Django 3.2 fix

--- api/controllers/OrdersImporter/apps.py
+++ api/controllers/OrdersImporter/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class OrdersimporterConfig(AppConfig):
-    name = "OrdersImporter"
+    name = "controllers.OrdersImporter"
--- api/controllers/footprints/apps.py
+++ api/controllers/footprints/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FootprintsConfig(AppConfig):
-    name = "footprints"
+    name = "controllers.footprints"
