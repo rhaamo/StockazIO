6ab758c7111f20b7cf272ef4e2a93b5361870afd
Dashie <dashie@sigpipe.me>
2021-05-08 10:13:30 +0200

    Fix auto field for our apps

--- api/controllers/OrdersImporter/apps.py
+++ api/controllers/OrdersImporter/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class OrdersimporterConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = "controllers.OrdersImporter"
--- api/controllers/categories/apps.py
+++ api/controllers/categories/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CategoriesConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = "controllers.categories"
