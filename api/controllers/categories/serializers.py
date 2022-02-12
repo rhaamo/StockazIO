5e5c5c58d523a646fc015d1bacb97cd2176e3348
Dashie <dashie@sigpipe.me>
2022-02-12 10:12:45 +0100

    Fix swagger

--- api/controllers/categories/serializers.py
+++ api/controllers/categories/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from .models import Category
+from controllers.utils import RecursiveField
 
 
 class CategorySerializer(serializers.ModelSerializer):
     def get_parts_count(self, obj):
         return obj.parts_count
 
+    def __init__(self, *args, depth=0, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.depth = depth
+
     class Meta:
         model = Category
         fields = ["id", "name", "parts_count", "children"]
+
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.depth != 1:
+            fields["children"] = RecursiveField(many=True, required=False)
+        return fields
 
 
 CategorySerializer._declared_fields["parts_count"] = serializers.SerializerMethodField()
--- api/controllers/storage/serializers.py
+++ api/controllers/storage/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import StorageCategory, StorageLocation
-
+from controllers.utils import RecursiveField
 
 class StorageLocationSerializer(serializers.ModelSerializer):
     picture_medium = serializers.ImageField(read_only=True)
@@ -20,10 +20,19 @@ class StorageLocationSerializer(serializers.ModelSerializer):
 class StorageSerializer(serializers.ModelSerializer):
     storage_locations = StorageLocationSerializer(many=True, read_only=True)
 
+    def __init__(self, *args, depth=0, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.depth = depth
+
     class Meta:
         model = StorageCategory
         fields = ["id", "name", "children", "storage_locations"]
 
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.depth != 1:
+            fields["children"] = RecursiveField(many=True, required=False)
+        return fields
 
 StorageSerializer._declared_fields["children"] = StorageSerializer(
     many=True,
