ad38bb32b1357f15f4c83a7c9a99bfae7c335082
Dashie <dashie@sigpipe.me>
2022-02-12 10:19:58 +0100

    And let swagger broken for now

--- api/controllers/categories/serializers.py
+++ api/controllers/categories/serializers.py
@@ -7,23 +7,13 @@ class CategorySerializer(serializers.ModelSerializer):
     def get_parts_count(self, obj):
         return obj.parts_count
 
-    def __init__(self, *args, depth=0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.depth = depth
-
     class Meta:
         model = Category
         fields = ["id", "name", "parts_count", "children"]
 
-    def get_fields(self):
-        fields = super().get_fields()
-        if self.depth != 1:
-            fields["children"] = RecursiveField(many=True, required=False)
-        return fields
 
 
 CategorySerializer._declared_fields["parts_count"] = serializers.SerializerMethodField()
-
 
 CategorySerializer._declared_fields["children"] = CategorySerializer(
     many=True,
