fb8077807e8fd1a934a20843718b08200760022d
Dashie <dashie@sigpipe.me>
2022-02-15 11:10:05 +0100

    Fix parts list

--- front/src/views/parts/List.vue
+++ front/src/views/parts/List.vue
@@ -11,7 +11,7 @@
     <div class="row">
       <div class="col-12">
         <ol class="breadcrumb">
-          <template v-if="categoryId != 0">
+          <template v-if="categoryId && category != 0">
             <li class="breadcrumb-item">
               Parts by category
             </li>
