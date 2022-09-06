7b3c7d44b63809fd12b16f076b0c01049b3b692d
Dashie <dashie@sigpipe.me>
2022-09-06 08:51:52 +0200

    Add Axios interceptor for bearer token

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
+import axios from "axios";
+import { useOauthStore } from "@/stores/oauth";
 
 import App from "./App.vue";
 import router from "./router";
@@ -16,5 +18,23 @@ pinia.use(
 
 app.use(pinia);
 app.use(router);
+
+const oauthStore = useOauthStore();
+
+axios.interceptors.request.use(
+  function (config) {
+    if (oauthStore.userToken) {
+      console.log("Axios interceptor set");
+      config.headers["Authorization"] = `Bearer ${oauthStore.userToken}`;
+    } else {
+      console.log("No Axios interceptor to set");
+    }
+    return config;
+  },
+  function (error) {
+    console.log("Cannot set Axios Interceptor for Auth token");
+    return Promise.reject(error);
+  }
+);
 
 app.mount("#app");
