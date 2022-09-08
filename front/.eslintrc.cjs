7bbc44b2108b48cab63492942bcfb27a2d8010df
Dashie <dashie@sigpipe.me>
2022-09-08 18:51:12 +0200

    fix one issue

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -3,6 +3,10 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  "env": {
+    "node": true,
+    "commonjs": true
+  },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   parserOptions: {
     ecmaVersion: "latest",
