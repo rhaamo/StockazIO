29bbdb3198e5cb3c090e5320e1169144145ad9e7
Dashie <dashie@sigpipe.me>
2022-10-12 19:21:40 +0200

    v1.1.0

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace("-", ".", 1).split(".")])
