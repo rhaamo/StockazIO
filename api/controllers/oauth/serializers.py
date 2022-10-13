08a2d1e19b1ed2c6a5a037087fecbc4a586b2038
Dashie <dashie@sigpipe.me>
2022-10-13 16:44:28 +0200

    fix oauth for new django oauth toolkit

--- api/controllers/oauth/serializers.py
+++ api/controllers/oauth/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from . import models
+from controllers.oauth import models
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
@@ -18,6 +18,23 @@ class ApplicationSerializer(serializers.ModelSerializer):
 class CreateApplicationSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=True, max_length=255)
     scopes = serializers.CharField(source="scope", default="read")
+
+    def create(self, validated_data):
+        # Due to django-oauth-toolkit change https://github.com/jazzband/django-oauth-toolkit/releases/tag/2.1.0
+        # we need to grab the client_secret before it is saved (and then hashed)
+        app = models.Application(**validated_data)
+        secret = app.client_secret
+        scopes = app.scope
+        app.save()
+        return {
+            "client_id": app.client_id,
+            "client_secret": secret,
+            "redirect_uris": app.redirect_uris,
+            "name": app.name,
+            "scopes": scopes,
+            "created": app.created,
+            "updated": app.updated,
+        }
 
     class Meta:
         model = models.Application
