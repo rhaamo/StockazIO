6c0f14ffab1db765c4d02d0c505fa6cab18674a5
Dashie <dashie@sigpipe.me>
2024-12-23 16:30:31 +0100

    margin

--- front/src/views/projects/List.vue
+++ front/src/views/projects/List.vue
@@ -111,7 +111,7 @@
           </template>
         </Column>
 
-        <Column headerStyle="width: 6em">
+        <Column headerStyle="width: 6.3em">
           <template #body="slotProps">
             <span class="p-buttonset">
               <PvButton
@@ -124,7 +124,7 @@
               <PvButton
                 type="button"
                 icon="fa fa-trash-o"
-                class="p-button-danger"
+                class="p-button-danger ml-1"
                 v-tooltip="'delete'"
                 @click="deleteItem($event, slotProps.data)"
               ></PvButton>
